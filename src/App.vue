597face09e3b5704cdc66b398d2a21190f63fd32
vertyll <gawrmiko@gmail.com>
2023-03-08 10:30:37 +0100

    main and vue router

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="sidebar">
+        <RouterLink to="/">Strona głowna</RouterLink>
+        <RouterLink to="/form">Formularz</RouterLink>
+        <RouterLink to="/posts">Posty</RouterLink>
+        <RouterLink to="/albums">Albumy</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
