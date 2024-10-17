6eb1465c77a3878cf577b1316c57066d52be2577
Mikolaj Gawron <gawrmiko@gmail.com>
2024-10-17 19:39:41 +0200

    feat: add loading state to photos view

--- src/views/PhotosView.vue
+++ src/views/PhotosView.vue
@@ -1,23 +1,28 @@
 <template>
   <main>
-    <BuildPhotos :photos="photos" v-if="photos" />
+    <div class="spinner" v-if="isLoading"></div>
+    <BuildPhotos :photos="photos" v-if="photos && !isLoading" />
   </main>
 </template>
 
 <script>
 import BuildPhotos from "@/components/organisms/BuildPhotos.vue";
 import { getPhotosFromAlbums } from "@/api.js";
+
 export default {
   data() {
     return {
       photos: null,
+      isLoading: false,
     };
   },
   components: {
     BuildPhotos,
   },
   async created() {
+    this.isLoading = true;
     this.photos = await getPhotosFromAlbums(this.$route.params.albumId);
+    this.isLoading = false;
   },
 };
 </script>
