d19a03bb2d08e95d5fa4319736e2cb8364f8da2f
Mikolaj Gawron <gawrmiko@gmail.com>
2024-10-17 19:05:49 +0200

    feat: improve style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
-  <main>
-    <p>Strona głowna</p>
+  <main class="home-page">
+    <div class="container-home-page">
+      <h1>Strona główna</h1>
+      <p>
+        Ta aplikacja korzysta z <a href="https://jsonplaceholder.typicode.com/" target="_blank">JSONPlaceholder API</a>
+        do pobierania przykładowych danych, takich jak posty, komentarze, użytkownicy i inne.
+      </p>
+      <p>Znajdziesz tutaj przykładowe dane do testowania aplikacji i nauki API.</p>
+    </div>
   </main>
 </template>
